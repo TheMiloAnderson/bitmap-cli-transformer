f74f48ea02c2bf15646ad41d4a0e23327f9b3135
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-22 17:14:16 -0800

    minor cleanup

--- cli.py
+++ cli.py
@@ -1,4 +1,3 @@
-import cmd
 import sys
 from modify import Bitmap
 
@@ -9,7 +8,7 @@ print('Argument List:', str(sys.argv))
 if len(sys.argv) < 3:
     print("""
     *************************************************
-    ***    Not enough arguments. Please enter    ***
+    ***    Not enough arguments. Please enter     ***
     ***  the file path and at least 1 transform   ***
     ***     (make_grayscale, make_thumbnail,      ***
     ***     flip_vertical, flip_horizontal, or    ***
@@ -19,7 +18,7 @@ if len(sys.argv) < 3:
 else:
     input_file_path = sys.argv[1]
     transforms = sys.argv[2:]
-    print('You have successfully modified' + input_file_path + '.')
+    print('You have successfully modified: ' + input_file_path + '.')
 
 
 bitmap = Bitmap(input_file_path)
@@ -27,4 +26,3 @@ bitmap.make_grayscale()
 for transform in transforms:
     method = getattr(bitmap, transform)
     bitmap.save_new(method())
-
