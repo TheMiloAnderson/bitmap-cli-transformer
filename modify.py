8ba1cf55693df66f2ba65420b8446a6d4a32d543
Dan Le <Danhuyle508@gmail.com>
2019-02-22 16:29:09 -0800

    docstrings added to test file and bitmap file

--- modify.py
+++ modify.py
@@ -17,31 +17,49 @@ class Bitmap(object):
         """
 
     def new_file_path(self, mod_type):
+        """
+        Create a new file path with the mod type
+        """
         split_name = self.file_path.split('.')
         split_name[1] += f'{mod_type}'
         return '.'.join(split_name)
 
     def make_grayscale(self, img='self.img'):
+        """
+        Modify the input image with grayscale  and returns the new path with the new image
+        """
         new_path = self.new_file_path('_grayscale')
         img = self.img.convert('L')
         return [new_path, img]
 
     def flip_horizontal(self, img='self.img'):
+        """
+        Modify the input image with flip horizontal  and returns the new path with the new image
+        """
         new_path = self.new_file_path('_horizontal')
         img = self.img.transpose(Image.FLIP_LEFT_RIGHT)
         return [new_path, img]
 
     def flip_vertical(self, img='self.img'):
+        """
+        Modify the input image with flip vertical  and returns the new path with the new image
+        """
         new_path = self.new_file_path('_vertical')
         img = self.img.transpose(Image.FLIP_TOP_BOTTOM)
         return [new_path, img]
 
     def invert_colors(self, img='self.img'):
+        """
+        Modify the input image with invert colors  and returns the new path with the new image
+        """
         new_path = self.new_file_path('_invert')
         img = ImageOps.invert(self.img)
         return [new_path, img]
 
     def make_thumbnail(self, img='self.img'):
+        """
+        Modify the input image into a thumbnail  and returns the new path with the new image
+        """
         new_path = self.new_file_path('_thumbnail')
         size = 128, 128
         img = self.img.copy()
@@ -49,6 +67,9 @@ class Bitmap(object):
         return [new_path, img]
 
     def save_new(self, lst):
+        """
+        Save the modified image into the folder
+        """
         lst[1].save(lst[0], 'bmp')
 
 if __name__ == "__main__":
