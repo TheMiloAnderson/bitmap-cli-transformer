6398c6b900bbadf4fc7f5f84d3a0c79c856bb65a
Dan Le <Danhuyle508@gmail.com>
2019-02-22 16:43:39 -0800

    messgaes updated

--- cli.py
+++ cli.py
@@ -10,12 +10,16 @@ if len(sys.argv) < 3:
     print("""
     *************************************************
     ***    Not enough parameters. Please enter    ***
-    ***  the file path and at least 1 transform.  ***
+    ***  the file path and at least 1 transform   ***
+    ***     (make_grayscale, make_thumbnail,      ***
+    ***     flip_vertical, flip_horizontal, or    ***
+    ***           invert_colors).                 ***
     *************************************************
     """)
 else:
     input_file_path = sys.argv[1]
     transforms = sys.argv[2:]
+    print(f'You have successfully modified {input_file_path}.')
 
 
 bitmap = Bitmap(input_file_path)
