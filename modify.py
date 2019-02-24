0851e8cdacc34bfd2e0d7b39aa444da71564f14e
TheMiloAnderson <themiloanderson@gmail.com>
2019-02-24 14:16:14 -0800

    custom grayscale is now mostly working

--- modify.py
+++ modify.py
@@ -9,6 +9,8 @@ class Bitmap(object):
         """
         self.file_path = file_path
         self.img = Image.open(self.file_path)
+        with open(file_path, 'rb') as file:
+            self.bin_img = bytearray(file.read())
 
     def new_file_path(self, mod_type):
         """
@@ -25,6 +27,42 @@ class Bitmap(object):
         new_path = self.new_file_path('_grayscale')
         img = self.img.convert('L')
         return [new_path, img]
+
+    def _make_bi_int(self, byts):
+        return int.from_bytes(byts, byteorder='little')
+
+    def custom_grayscale(self, img='self.img'):
+        new_path = self.new_file_path('_grayscale')
+
+        file_header = self.bin_img[:14]
+        img_header_size = self._make_bi_int(self.bin_img[14:18])
+        img_header = self.bin_img[14:14 + img_header_size]
+        img_pix_location = self._make_bi_int(file_header[10:])
+        img_pix = self.bin_img[img_pix_location:]
+        img_width = self._make_bi_int(img_header[4:6])
+        img_height = self._make_bi_int(img_header[8:10])
+        img_bpp = self._make_bi_int(img_header[14:16])
+        img_cols = img_width * 3
+        img_cols_pad = ((img_cols + 3) & ~0x03) - img_cols
+
+        counter = 0
+        for i in range(0, len(img_pix) - 1):
+            if counter < img_cols and counter % 3 == 0:
+                b = img_pix[i]
+                g = img_pix[i + 1]
+                r = img_pix[i + 2]
+                avg = int(round((r + g + b) / 3))
+                img_pix[i] = avg
+                img_pix[i + 1] = avg
+                img_pix[i + 2] = avg
+            if counter == img_cols + img_cols_pad:
+                counter = 0
+            else:
+                counter += 1
+
+        new_bitmap = file_header + img_header + img_pix
+        return [new_bitmap, new_path]
+
 
     def flip_horizontal(self, img='self.img'):
         """
@@ -66,12 +104,11 @@ class Bitmap(object):
         """
         lst[1].save(lst[0], 'bmp')
 
+    def save_custom(self, img_path):
+        with open(img_path[1], 'wb') as file:
+            file.write(img_path[0])
+
 if __name__ == "__main__":
 
-    tiger = Bitmap('./assets/tiger.bmp')
-
-    tiger.save_new(tiger.make_grayscale())
-    tiger.save_new(tiger.flip_horizontal())
-    tiger.save_new(tiger.flip_vertical())
-    tiger.save_new(tiger.make_thumbnail())
-    tiger.save_new(tiger.invert_colors())
+    test = Bitmap('./assets/spock.bmp')
+    test.custom_grayscale()
